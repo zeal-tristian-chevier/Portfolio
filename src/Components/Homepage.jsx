1a97a77f216c9a55b042bbe8f33202e5636a9663
Tristian Chevier <80795010+tchevier@users.noreply.github.com>
2022-10-28 15:20:22 -0600

    initial commit

---
+++ src/Components/Homepage.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import '../static/styles/Homepage.css';
+
+const Homepage = () => {
+
+    return (
+        <>
+        <div className="row d-flex justify-content-center">
+            <div className="col-lg-6">
+                <div className="text-start">
+                    <h1>About</h1>
+                    <p>My name is Tristian Chevier and I am a Full-Stack developer living in Aurora, Colorado.</p>
+                    <h2>How I learned how to code</h2>
+                    <p>After learning a bit of Java in Highschool, I became inspired to developing more with code.</p>
+                    <p>I spent a few months watching Udemy courses on creating websites because I had always wanted to create websites for the businesses local to me. I believe this is my way of helping those around me.</p>  
+                    <p>Unfortunately, I became stuck in the infinite tutorial loop. I wasn't going to give up so I decided to join Coding Dojo.</p>
+                    <h1>My Projects</h1>
+                    <h2>Flak Official - Python, Flask, MySQL</h2>
+                    <p> A Band Promotion Website designed for a local band to promote previous shows and sell merchandise.</p>
+                    <a href="https://flakofficial.herokuapp.com/" target="_blank" rel="noreferrer noopener">
+                    <img src={require('../static/images/Flak.png')} alt="" data-target="animated-image.originalImage" />
+                    </a>
+                    <h2>Chev's Garage - MERN</h2>
+                    <p>An ATV Repair website for a real company in Haslet, Texas made to allow customers contact the owner and display his past projects.</p>
+                    <a href="http://chevsgarage.com/" target="_blank" rel="noreferrer noopener">
+                    <img src={require('../static/images/Chev.png')} alt="" data-target="animated-image.originalImage" />
+                    </a>
+                    <h2>PokeMERN - React</h2>
+                    <p>A Pokemon battle remake system. Choose a pokemon face a random enemy pokemon. Made in ReactJs</p>
+                    <img src={require('../static/images/PokeMERN2.gif')} alt="" data-target="animated-image.originalImage" />
+                    <h2>Tire Way - C#, MySQL</h2>
+                    <p>A Browser Dealership software designed to handle customer tire orders.</p>
+                    <a href="https://github.com/tchevier/tire-way" target="_blank" rel="noreferrer noopener">
+                    <img src={require('../static/images/Tire.png')} alt="" data-target="animated-image.originalImage" />
+                    </a>
+                </div>
+            </div>
+        </div>
+        
+        </>
+    )
+}
+export default Homepage;

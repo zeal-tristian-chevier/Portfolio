fbc9273eb419ebe6f571e908b598df24e0a45f97
Tristian Chevier <80795010+tchevier@users.noreply.github.com>
2023-04-04 18:22:12 -0600

    file management

--- src/App.js
+++ src/App.js
@@ -1,53 +1,118 @@
-import './App.css';
-import Homepage from './Components/Homepage';
-
+import "./App.css";
+import Homepage from "./components/homepage";
+const Navbar = () => {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg d-flex justify-content-between">
+                <div className="container">
+                    <a className="navbar-brand ms-5" href="#">
+                        <img
+                            src={require("./static/images/TrizyLogoSmall.png")}
+                            alt="Logo"
+                            width="75"
+                            height="100"
+                        ></img>
+                    </a>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNavDropdown"
+                        aria-controls="navbarNavDropdown"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div
+                        className="collapse navbar-collapse me-5 justify-content-end"
+                        id="navbarNavDropdown"
+                    >
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a
+                                    className="nav-link"
+                                    aria-current="page"
+                                    href="https://www.linkedin.com/in/tristian-chevier-61839a249/"
+                                    target="_blank"
+                                    rel="noreferrer noopener"
+                                >
+                                    linkedIn{" "}
+                                    <i className="fab fa-linkedin-in"></i>
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a
+                                    className="nav-link"
+                                    href="https://github.com/tchevier"
+                                    target="_blank"
+                                    rel="noreferrer noopener"
+                                >
+                                    github <i className="fab fa-github"></i>
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a
+                                    className="nav-link"
+                                    href="mailto:tchevier3@gmail.com"
+                                    target="_blank"
+                                    rel="noreferrer noopener"
+                                >
+                                    email <i className="fab fa-google"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+};
+const Footer = () => {
+    return (
+        <>
+            <footer className="text-center text-white">
+                <div className="container p-4 pb-0">
+                    <section className="mb-3">
+                        <a
+                            className="btn btn-outline-light btn-floating m-1"
+                            href="https://www.linkedin.com/in/tristian-chevier-61839a249/"
+                            target="_blank"
+                            rel="noreferrer noopener"
+                        >
+                            <i className="fab fa-linkedin-in"></i>
+                        </a>
+                        <a
+                            className="btn btn-outline-light btn-floating m-1"
+                            href="https://github.com/tchevier"
+                            target="_blank"
+                            rel="noreferrer noopener"
+                        >
+                            <i className="fab fa-github"></i>
+                        </a>
+                        <a
+                            className="btn btn-outline-light btn-floating m-1"
+                            href="mailto:tchevier3@gmail.com"
+                            target="_blank"
+                            rel="noreferrer noopener"
+                        >
+                            <i className="fab fa-google"></i>
+                        </a>
+                    </section>
+                </div>
+                <div className="text-center p-3">© 2022 - Tristian Chevier</div>
+            </footer>
+        </>
+    );
+};
 function App() {
-  return (
-    <>
-      <nav className="navbar navbar-expand-lg d-flex justify-content-between">
-        <div className="container">
-        <a className="navbar-brand ms-5" href="#">
-          <img src={require('./static/images/TrizyLogoSmall.png')} alt="Logo" width="75" height="100">
-          </img>
-          </a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse me-5 justify-content-end" id="navbarNavDropdown">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a className="nav-link" aria-current="page" href="https://www.linkedin.com/in/tristian-chevier-61839a249/" target="_blank" rel="noreferrer noopener">linkedIn <i className="fab fa-linkedin-in"></i></a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="https://github.com/tchevier" target="_blank" rel="noreferrer noopener">github <i className="fab fa-github"></i></a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="mailto:tchevier3@gmail.com" target="_blank" rel="noreferrer noopener">email <i className="fab fa-google"></i></a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-    <div className="App">
-      <Homepage />
-    </div>
-    <footer className="text-center text-white">
-      <div className="container p-4 pb-0">
-        <section className="mb-3">
-          <a className="btn btn-outline-light btn-floating m-1" href="https://www.linkedin.com/in/tristian-chevier-61839a249/" target="_blank" rel="noreferrer noopener"><i className="fab fa-linkedin-in"></i
-          ></a>
-          <a className="btn btn-outline-light btn-floating m-1" href="https://github.com/tchevier" target="_blank" rel="noreferrer noopener"><i className="fab fa-github"></i
-          ></a>
-          <a className="btn btn-outline-light btn-floating m-1" href="mailto:tchevier3@gmail.com" target="_blank" rel="noreferrer noopener"><i className="fab fa-google"></i
-          ></a>
-        </section>
-      </div>
-      <div className="text-center p-3">
-        © 2022 - Tristian Chevier
-      </div>
-    </footer>
-    </>
-  );
+    return (
+        <>
+            <Navbar />
+            <Homepage />
+            <Footer />
+        </>
+    );
 }
 
 export default App;
--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,59 +1,52 @@
-import React from 'react'
-import '../static/styles/Homepage.css';
+import React from "react";
+import "../static/styles/Homepage.css";
+import Projects from "./projects";
+
+const AboutSection = () => {
+    return (
+        <>
+            <h1>About</h1>
+            <p>
+                My name is Tristian Chevier and I am a Full-Stack developer
+                living in Aurora, Colorado.
+            </p>
+            <h2>How I learned to code</h2>
+            <p>
+                After learning a bit of Java in High School, I became inspired
+                to developing more with code.
+            </p>
+            <p>
+                I spent a few months watching Udemy courses on creating websites
+                because I had always wanted to create websites for the
+                businesses local to me. I believe this is my way of helping
+                those around me.
+            </p>
+            <p>
+                Unfortunately, I became stuck in the infinite tutorial loop. I
+                wasn't going to give up so I decided to join Coding Dojo.
+            </p>
+            <p>
+                Coding Dojo taught me the ins and outs of Full-Stack
+                development. Below I have listed all of my projects from each
+                stack.
+            </p>
+        </>
+    );
+};
 
 const Homepage = () => {
 
     return (
         <>
-        <div className="row d-flex justify-content-center">
-            <div className="col-lg-6">
-                <div className="text-start">
-                    <h1>About</h1>
-                    <p>My name is Tristian Chevier and I am a Full-Stack developer living in Aurora, Colorado.</p>
-                    <h2>How I learned to code</h2>
-                    <p>After learning a bit of Java in High School, I became inspired to developing more with code.</p>
-                    <p>I spent a few months watching Udemy courses on creating websites because I had always wanted to create websites for the businesses local to me. I believe this is my way of helping those around me.</p>  
-                    <p>Unfortunately, I became stuck in the infinite tutorial loop. I wasn't going to give up so I decided to join Coding Dojo.</p>
-                    <p>Coding Dojo taught me the ins and outs of Full-Stack development. Below I have listed all of my projects from each stack.</p>
-                    <h1>My Projects</h1>
-                    <h2>PickUpPro - C#, MySQL</h2>
-                    <p>A website created to allow parents to easily pick up their kids from school without the headache of trying to find them. Admins will wait with the students until parents request to pickup their child.</p>
-                    <img onClick={() => alert("The project is private")} src={require('../static/images/PickUpPro.png')} alt="" data-target="animated-image.originalImage" />
-                    <h2>Code Crackdown - MERN</h2>
-                    <p>A game based off the board game "Mastermind" with implemented User login and registration as well as custom scoring algorithm to allow users to participate on the leaderboard.</p>
-                    <a href="https://www.codecrackdown.com/" target="_blank" rel="noreferrer noopener">
-                    <img src={require('../static/images/CodeCrackdown.png')} alt="" data-target="animated-image.originalImage" />
-                    </a>
-                    <h2>Flak Official - Python, Flask, MySQL</h2>
-                    <p> A Band Promotion Website designed for a local band to promote previous shows and sell merchandise, using the Stripe API.</p>
-                    <a href="https://github.com/tchevier/Flak-Official" target="_blank" rel="noreferrer noopener">
-                    <img src={require('../static/images/Flak.png')} alt="" data-target="animated-image.originalImage" />
-                    </a>
-                    <h2>Chev's Garage - MERN</h2>
-                    <p>An ATV Repair website for a real company in Haslet, Texas made to allow customers contact the owner and display his past projects.</p>
-                    <a href="http://chevsgarage.com/" target="_blank" rel="noreferrer noopener">
-                    <img src={require('../static/images/Chev.png')} alt="" data-target="animated-image.originalImage" />
-                    </a>
-                    <h2>PokeMERN - React</h2>
-                    <p>A Pokemon battle remake system using the Pokemon API. Choose a pokemon face a random enemy pokemon.</p>
-                    <a href="https://github.com/SeanMcNamee86/pokemern/tree/dev-sp" target="_blank" rel="noreferrer noopener">
-                    <img src={require('../static/images/PokeMERN2.gif')} alt="" data-target="animated-image.originalImage" />
-                    </a>
-                    <h2>Tire Way - C#, MySQL</h2>
-                    <p>A Browser Dealership software designed to handle customer tire orders.</p>
-                    <a href="https://github.com/tchevier/tire-way" target="_blank" rel="noreferrer noopener">
-                    <img src={require('../static/images/Tire.png')} alt="" data-target="animated-image.originalImage" />
-                    </a>
-                    <h2>Moovies - React</h2>
-                    <p>Mini Netflix clone that calls from a movie API and displays it in React and has a fully functioning search-bar.</p>
-                    <a href="https://charter-spectrum-coding-challenge.vercel.app/" target="_blank" rel="noreferrer noopener">
-                    <img src={require('../static/images/Moovies.png')} alt="" data-target="animated-image.originalImage" />
-                    </a>
+            <div className="row d-flex justify-content-center">
+                <div className="col-lg-6">
+                    <div className="text-start">
+                        <AboutSection />
+                        <Projects />
+                    </div>
                 </div>
             </div>
-        </div>
-        
         </>
-    )
-}
+    );
+};
 export default Homepage;
